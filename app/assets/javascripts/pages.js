b23d1d795d5503719eb73248c02ff73b6e10c6dc
Kevin Kang <kevinkang88@gmail.com>
2014-07-26 23:39:06 -0700

    added usename and password to database

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -3,6 +3,7 @@ var playSong = function(){
     // play Yiruma song!
     var params = { allowScriptAccess: "always" };
     var atts = { id: "myytplayer" };
+
     if($('#cram-amt').val() == 'easy-pz'){
       $.ajax({
         url: "/song_maker/level_one",
@@ -10,21 +11,73 @@ var playSong = function(){
         dataType: "json",
         success: function(data){
           console.log("works!");
-          // here data contains hash with video IDs as values
           var videoIdsWithNum = data;
-          var videoIds = [];
+          videoIds = [];
           for(var key in videoIdsWithNum){
             videoIds.push(videoIdsWithNum[key]);
+          }
+          swfobject.embedSWF("http://www.youtube.com/v/" + videoIds[3]+ "?enablejsapi=1&playerapiid=ytplayer&version=3&autoplay=1",
+                          "playerBox", "500", "500", "8", null, null, params, atts);
+          function ytplayerOnStateChange(event) {
+            if(event.data === 0) {
+              console.log("works man for reals");
+            }
           }
 
         }
       })
-      // swfobject.embedSWF("http://www.youtube.com/v/CE8vZEheWew?enablejsapi=1&playerapiid=ytplayer&version=3&autoplay=1",
-      //                    "playerBox", "200", "300", "8", null, null, params, atts);
     }
   })
 };
-//
+//++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+
+
+
+
+
+
+
+
+
+// $(document).ready(function() {
+//   $('#start').on('click', initializeVideo);
+// });
+
+// var initializeVideo = function() {
+//   hideButton();
+//   var params = { allowScriptAccess: "always" };
+//   var atts = { id: "myytplayer" };
+//   var videoID = "F1FMFBdse7g";
+//   swfobject.embedSWF("http://www.youtube.com/v/" + videoID + "?enablejsapi=1&playerapiid=ytplayer&version=3&autoplay=1",
+//                    "ytapiplayer", "500", "500", "8", null, null, params, atts);
+// };
+
+// var hideButton  = function() {
+//   $('#start').attr('class', 'hidden');
+// };
+
+
+// var queueNextSong = function(ytplayer) {
+//   ytplayer.cueVideoById("oS4lYcgtT4E");
+//   console.log("queued song")
+// };
+
+// function onYouTubePlayerReady(playerId) {
+//   ytplayer = document.getElementById("myytplayer");
+//   console.log(ytplayer)
+//   ytplayer.addEventListener("onStateChange", "ytplayerOnStateChange");
+// };
+
+// var ytplayerOnStateChange = function(state){
+//   console.log(state);
+//   if (state === 0) {
+//     alert("state is 0");
+//     queueNextSong(ytplayer);
+//     ytplayer.playVideo();
+//   }
+// };
 
 
 
@@ -37,7 +90,30 @@ var playSong = function(){
 
 
 
-//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++
 var setUpSlider = function(){
   var levels = ["easy-pz","moderate","must-cram!","so-stressed!"];
   $("#slider").slider({
