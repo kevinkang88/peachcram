1759ed6a37b5af887343b1d92e49e9e671063360
Kevin Kang <kevinkang88@gmail.com>
2014-07-27 09:58:30 -0700

    succesfully connected queue so that 2 videos consequitively pop up after anther

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -16,68 +16,47 @@ var playSong = function(){
           for(var key in videoIdsWithNum){
             videoIds.push(videoIdsWithNum[key]);
           }
-          swfobject.embedSWF("http://www.youtube.com/v/" + videoIds[3]+ "?enablejsapi=1&playerapiid=ytplayer&version=3&autoplay=1",
-                          "playerBox", "500", "500", "8", null, null, params, atts);
-          function ytplayerOnStateChange(event) {
-            if(event.data === 0) {
-              console.log("works man for reals");
-            }
-          }
+          initializeVideo(videoIds.shift());
+
+
+
 
         }
       })
     }
   })
 };
+
 //++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 
+function initializeVideo(videoID) {
+  var params = { allowScriptAccess: "always" };
+  var atts = { id: "myytplayer" };
+  swfobject.embedSWF("http://www.youtube.com/v/" + videoID + "?enablejsapi=1&playerapiid=ytplayer&version=3&autoplay=1",
+                   "ytapiplayer", "500", "500", "8", null, null, params, atts);
+};
+function ytplayerOnStateChange(state) {
+  console.log(state);
+  if (state === 0) {
+    alert("state is 0");
+    queueNextSong(ytplayer);
+    ytplayer.playVideo();
+  }
+};
+function onYouTubePlayerReady(playerId) {
+  ytplayer = document.getElementById("myytplayer");
+  console.log(ytplayer)
+  ytplayer.addEventListener("onStateChange", "ytplayerOnStateChange");
+};
+
+function queueNextSong(ytplayer) {
+  ytplayer.cueVideoById("oS4lYcgtT4E");
+  console.log("queued song")
+};
+//++++++++++++++++++++++++++++++++++++++
 
 
-
-
-
-
-
-
-
-// $(document).ready(function() {
-//   $('#start').on('click', initializeVideo);
-// });
-
-// var initializeVideo = function() {
-//   hideButton();
-//   var params = { allowScriptAccess: "always" };
-//   var atts = { id: "myytplayer" };
-//   var videoID = "F1FMFBdse7g";
-//   swfobject.embedSWF("http://www.youtube.com/v/" + videoID + "?enablejsapi=1&playerapiid=ytplayer&version=3&autoplay=1",
-//                    "ytapiplayer", "500", "500", "8", null, null, params, atts);
-// };
-
-// var hideButton  = function() {
-//   $('#start').attr('class', 'hidden');
-// };
-
-
-// var queueNextSong = function(ytplayer) {
-//   ytplayer.cueVideoById("oS4lYcgtT4E");
-//   console.log("queued song")
-// };
-
-// function onYouTubePlayerReady(playerId) {
-//   ytplayer = document.getElementById("myytplayer");
-//   console.log(ytplayer)
-//   ytplayer.addEventListener("onStateChange", "ytplayerOnStateChange");
-// };
-
-// var ytplayerOnStateChange = function(state){
-//   console.log(state);
-//   if (state === 0) {
-//     alert("state is 0");
-//     queueNextSong(ytplayer);
-//     ytplayer.playVideo();
-//   }
-// };
 
 
 
