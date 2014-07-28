2801d6380986c9cb46e712fa998efd98962e6267
Kevin Kang <kevinkang88@gmail.com>
2014-07-27 19:05:21 -0700

    created precompile false in the correct line of rb file

--- config/application.rb
+++ config/application.rb
@@ -14,11 +14,11 @@ if defined?(Bundler)
   # If you want your assets lazily compiled in production, use this line
   # Bundler.require(:default, :assets, Rails.env)
 end
-config.assets.initialize_on_precompile = false
 
 
 module Againpeach
   class Application < Rails::Application
+    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
